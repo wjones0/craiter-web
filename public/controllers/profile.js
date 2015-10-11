872029faf95386285f808d6afb46faa010af260a
William Jones <wjones0@users.noreply.github.com>
2015-10-11 18:41:36 -0400

    removed front end support for other auths

--- public/controllers/profile.js
+++ public/controllers/profile.js
@@ -6,24 +6,12 @@
                     templateUrl: '/views/profile.html',
                     controller: 'ProfileController',
                     controllerAs: 'profile'
-                }) 
-                .when('/connect/local', {
-                    templateUrl: '/views/connect-local.html',
-                    controller: 'SecondarySignupController',
-                    controllerAs: 'profile'
                 });
 
             $locationProvider.html5Mode(true);
         }])
         .controller('ProfileController', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams) {
             //Custom Profile functionality
-            $http.get('/api/userData')
-                .success(function(data) {
-                    $scope.user = data; //Expose the user data to your angular scope
-                });
-        }])
-        .controller('SecondarySignupController', ['$http', '$scope', '$routeParams', function($http, $scope, $routeParams) {
-            //Custom Link Page functionality
             $http.get('/api/userData')
                 .success(function(data) {
                     $scope.user = data; //Expose the user data to your angular scope
