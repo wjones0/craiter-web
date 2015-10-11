47dff72b8546893c00b360aedfa9f79e5205ce25
William Jones <wjones0@users.noreply.github.com>
2015-10-11 18:36:24 -0400

    remove backend support for other authentications

--- app/routes.js
+++ app/routes.js
@@ -4,77 +4,6 @@ module.exports = function (app, passport) {
 	
 	
 	
-	//  ---------  Login   /login -------------------
-	// app.get('/login', function(req,res) {
-	// 	res.render('login.ejs', {message: req.flash('loginMessage') });
-	// });
-
-	app.post('/login', function (req, res, next) {
-		if (!req.body.email || !req.body.password) {
-			return res.json({ error: 'Email and Password required' });
-		}
-		passport.authenticate('local-login', function (err, user, info) {
-			if (err) {
-				return res.json(err);
-			}
-			if (user.error) {
-				return res.json({ error: user.error });
-			}
-			req.logIn(user, function (err) {
-				if (err) {
-					return res.json(err);
-				}
-				return res.json({ redirect: '/profile' });
-			});
-		})(req, res);
-	});
-	
-	
-	
-	// ------------ Sign up   /signup ---------------------
-	// app.get('/signup', function(req,res) {
-	// 	res.render('signup.ejs', {message: req.flash('signupMessage') });
-	// });
-	
-	app.post('/signup', function (req, res, next) {
-		if (!req.body.email || !req.body.password) {
-			return res.json({ error: 'Email and Password required' });
-		}
-		passport.authenticate('local-signup', function (err, user, info) {
-			if (err) {
-				return res.json(err);
-			}
-			if (user.error) {
-				return res.json({ error: user.error });
-			}
-			req.logIn(user, function (err) {
-				if (err) {
-					return res.json(err);
-				}
-				return res.json({ redirect: '/profile' });
-			});
-		})(req, res);
-	});
-	
-	
-	
-	// ------------ Profile   /profile --------------------
-	// app.get('/profile', isLoggedIn, function(req,res) {
-	// 	res.render('profile.ejs', {
-	// 		user : req.user
-	// 	});
-	// });
-	
-	
-	// -------------  FACEBOOK -----------------------------
-	app.get('/auth/facebook', passport.authenticate('facebook', { scope: 'email' }));
-
-	app.get('/auth/facebook/callback',
-		passport.authenticate('facebook', {
-			successRedirect: '/profile',
-			failureRedirect: '/'
-		}));	
-	
 	
 	// ----------------- TWITTER ----------------------------
 	app.get('/auth/twitter', passport.authenticate('twitter'));
@@ -85,16 +14,6 @@ module.exports = function (app, passport) {
 			failureRedirect: '/'
 		})
 		);
-	
-	
-	// ------------------ GOOGLE ----------------------------
-	app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-	app.get('/auth/google/callback',
-		passport.authenticate('google', {
-			successRedirect: '/profile',
-			failureRedirect: '/'
-		}));
 	
 	
 	// ------------ Logout   /logout -----------------------
--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,5 @@
 
-var LocalStrategy = require('passport-local').Strategy;
-var FacebookStrategy = require('passport-facebook').Strategy;
 var TwitterStrategy = require('passport-twitter').Strategy;
-var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
 var User = require('../app/models/user');
 
@@ -25,138 +22,6 @@ module.exports = function (passport) {
 		});
 	});
 
-	//----------------------------LOCAL-----------------------
-	//  LOCAL SIGNUP
-	
-	passport.use('local-signup', new LocalStrategy({
-		usernameField: 'email',
-		passwordField: 'password',
-		passReqToCallback: true
-	},
-		function (req, email, password, done) {
-			process.nextTick(function () {
-				// if the user is not already logged in:
-				if (!req.user) {
-					User.findOne({ 'local.email': email }, function (err, user) {
-						// if there are any errors, return the error
-						if (err)
-							return done(err);
-
-						console.log(user);
-						// check to see if theres already a user with that email
-						if (user) {
-							return done(null, { error: 'That email is already taken.' });
-						} else {
-
-							// create the user
-							var newUser = new User();
-
-							newUser.local.email = email;
-							newUser.local.password = newUser.generateHash(password);
-
-							newUser.save(function (err) {
-								if (err)
-									throw err;
-
-								return done(null, newUser);
-							});
-						}
-
-					});
-					// if the user is logged in but has no local account...
-				} else if (!req.user.local.email) {
-					// ...presumably they're trying to connect a local account
-					var user = req.user;
-					user.local.email = email;
-					user.local.password = user.generateHash(password);
-					user.save(function (err) {
-						if (err)
-							throw err;
-						return done(null, user);
-					});
-				} else {
-					// user is logged in and already has a local account. Ignore signup. (You should log out before trying to create a new account, user!)
-					return done(null, req.user);
-				}
-
-			});
-
-		}
-		));
-	
-	
-	//  LOCAL LOGIN
-	
-	passport.use('local-login', new LocalStrategy({
-		usernameField: 'email',
-		passwordField: 'password',
-		passReqToCallback: true
-	},
-		function (req, email, password, done) {
-			// async
-			// user.findOne won't fire unless data is sent back
-			process.nextTick(function () {
-				// find a user for the email address
-				User.findOne({ 'local.email': email }, function (err, user) {
-					if (err)
-						return done(err);
-
-					if (!user)
-						return done(null, { error: 'No user found. ' });
-
-					if (!user.validPassword(password))
-						return done(null, { error: 'Incorrect password.' });
-					else
-						return done(null, user);
-
-				});
-			});
-		}
-		));
-	
-	
-	//---------------------FACEBOOK---------------------------
-	
-	passport.use(new FacebookStrategy({
-		clientID: configAuth.facebookAuth.clientID,
-		clientSecret: configAuth.facebookAuth.clientSecret,
-		callbackURL: configAuth.facebookAuth.callbackURL
-	},
-	
-		//  facebook will send back the token and profile
-		function (token, refreshToken, profile, done) {
-		
-			// async
-			process.nextTick(function () {
-			
-				// find the user
-				User.findOne({ 'facebook.id': profile.id }, function (err, user) {
-					if (err)
-						return done(err);
-
-					if (user) {
-						return done(null, user);
-					} else {
-						var newUser = new User();
-
-						newUser.facebook.id = profile.id;
-						newUser.facebook.token = token;
-						newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
-						newUser.facebook.email = profile.emails[0].value;
-
-						newUser.save(function (err) {
-							if (err)
-								throw err;
-
-							return done(null, newUser);
-						});
-					}
-
-				});
-			});
-		}
-
-		));
 	
 	
 	// --------------------- TWITTER ------------------------
@@ -193,39 +58,5 @@ module.exports = function (passport) {
 		}
 		));
 	
-	
-	
-	// ---------------------------- GOOGLE -------------------------
-	
-	passport.use(new GoogleStrategy({
-		clientID: configAuth.googleAuth.clientID,
-		clientSecret: configAuth.googleAuth.clientSecret,
-		callbackURL: configAuth.googleAuth.callbackURL
-	},
-		function (token, refreshToken, profile, done) {
-			process.nextTick(function () {
-				User.findOne({ 'google.id': profile.id }, function (err, user) {
-					if (err)
-						return done(err);
 
-					if (user) {
-						return done(null, user);
-					} else {
-						var newUser = new User();
-
-						newUser.google.id = profile.id;
-						newUser.google.token = token;
-						newUser.google.name = profile.displayName;
-						newUser.google.email = profile.emails[0].value;
-
-						newUser.save(function (err) {
-							if (err)
-								throw err;
-							return done(null, newUser);
-						});
-					}
-				});
-			});
-		}
-		));
 };
